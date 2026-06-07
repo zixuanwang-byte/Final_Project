8ded0915b61c1f11eb7bd7c82bb23de145362501
twishha6272727 <twishhamayur.patel1@rbe.sk.ca>
2026-06-07 10:28:57 -0600

    change

---
+++ story_line.py
@@ -0,0 +1,6 @@
+#story line for version 1
+print("The game name is Labubu and Minion's War bettle. ")
+print("When the minions go to meet their new boss to the New york.")
+print("Zimomo attacks on the minion's house to make his kingdom big.")
+print("Zimomo takes over the Minion's house. When minion know that they"
+    +" are ready to do battle with Zimomo.")
