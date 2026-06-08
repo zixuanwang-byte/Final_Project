6c03a694f40150b942dcad8080892371dc7ef72b
twishha6272727 <twishhamayur.patel1@rbe.sk.ca>
2026-06-08 15:09:19 -0600

    change the errors

--- battle_function.py
+++ battle_function.py
@@ -1,17 +1,26 @@
-# battle functions
-def battle(player, enemy):
-    print(f"{player.name} is battling with {enemy.name}.")
-    while self.health > 0 and enemy.health > 0 :
-        enemy.health -= self.attacks
-        print(f"{self.name} do the attacks on {enemy.name}.")
+from character import Player, Enemy 
+from moves import Moves 
 
-        if enemy.health <= 0:
-            print(f"{enemy.name} was loose in this battle.")
+
+Minion = Player("minion", True, 10, 10, 1)
+Labubu = Enemy("Labubu", True, 10, 10, 1)
+
+
+# battle functions
+def battle(self, Player, Enemy):
+    print(f"{Player.name} is battling with {Enemy.name}.")
+    while self.health > 0 and Enemy.health > 0 :
+        Enemy.health -= self.attack
+        print(f"{self.name} do the attacks on {Enemy.name}.")
+
+        if Enemy.health <= 0:
+            print(f"{Enemy.name} was loose in this battle.")
             break
         
-        self.health -= enemy.attack
-        print(f"{enemy.name} attacks on you.")
+        self.health -= Enemy.attack
+        print(f"{Enemy.name} attacks on you.")
 
         if self.health <= 0:
             print(f"{self.name} was loose in this battle.")
-    
+
+battle(Player, Minion, Labubu)
