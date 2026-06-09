8341649bad19925fd7f58d17488ed1cbad6b346b
twishha6272727 <twishhamayur.patel1@rbe.sk.ca>
2026-06-08 21:35:07 -0600

    change

--- battle_function.py
+++ battle_function.py
@@ -1,3 +1,4 @@
+from character import Player, Enemy
 # battle functions
 def battle(player, enemy):
     print(f"{player.name} is battling with {enemy.name}.")
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from character import Player, Enemy
 from moves import Moves
 
-# Test code for classes. Comment out or delete later.
+'''# Test code for classes. Comment out or delete later.
 move1 = Moves("test move 1", [1, 5])
 move2 = Moves("test move 2", [2, 3])
 test_moves_player = {"move 1": move1, "move 2": move2}
@@ -13,4 +13,7 @@ test_char1.use_move(test_char2)
 test_char2.attack(test_char1)
 
 test_char1.view_health()
-test_char2.view_health()
+test_char2.view_health()'''
+
+minion_banana = ["Fire Banana", "Water Banana", "Iron Fail Banana", "Cannon Banana", "Bubble Banana"]
+Labubu_ball = ["Bubble ball", "Iron Fail ball", "Cannon ball", "Fire ball", "Water ball"]
