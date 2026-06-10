b3dc1517047e4057d896e4dd7de35942c375ebd7
twishha6272727 <twishhamayur.patel1@rbe.sk.ca>
2026-06-09 21:35:08 -0600

    change

--- main.py
+++ main.py
@@ -30,6 +30,22 @@ attacks = {
         "Fire ball", 
         "Water ball"]}
 
+Labubu_attacks = {"Fire": Fire ball, "Bubble": Bubble ball, "Iron": Iron Fail ball,
+                 "Cannon": Cannon ball, "Water": Water ball}
+minion_attacks = {"Bubble": Bubble Banana, "Water": Water Banana, "Iron": Iron Fail Banana,
+                 "Fire": Fire Banana, "Cannon": Cannon Banana}
+
+def battle_atttacks():
+    print("\n Which Banana type you like to use for attack?")
+    for option in minion_attacks:
+        print(f" - {option}")
+    choice = input("choice: ")
+    if choice in minion_attacks:
+        minion_attacks[choice]()
+    else:
+        print("Sorry this option is not valid. Please try again.")
+print(battle_attacks)
+
 def move(self, direction):
     if direction == "north":
         self.x += 1
