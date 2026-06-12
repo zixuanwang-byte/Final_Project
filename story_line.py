5cd2e69ed6cf114045bf7ab684856192b91a7b2c
twishha6272727 <twishhamayur.patel1@rbe.sk.ca>
2026-06-12 15:08:53 -0600

    story for version2

--- story_line.py
+++ story_line.py
@@ -1,7 +1,29 @@
-#story line for version 1
-def introduction_storyline():
-    print("The game name is Labubu and Minion's War Battle. ")
-    print("When the minions go to meet their new boss at the New york."
-          + " Zimomo attacks on the minion's house to make his kingdom big.")
-    print("Zimomo takes over the Minion's house. When minion know that they"
-        +" are ready to do battle with Zimomo.")
+#Stroy of version 2.0 
+def story_lines2():
+    print("This game's name is Labubu and Minion War Battle.")
+    print("This story is about the war between the minions and Labubus.")
+    print("One minion's name was Kevin, he decided to go"
+          + " to New York city to find the baddest villain in the world to save"
+          + " his people.")
+    print("")
+    choice = input("Do you want to continue to read the story? (yes/no): ")
+    if choice.lower() == "yes":
+        print()
+        print("Kevin took Stuart and Bob with him. They found their boss, she is"
+              + " the worlds top female supervillain named Scarlett Overkill."
+              + " They send the message tp their family."
+              + " The whole minion group goes to the"
+              + " city to see their new boss.")
+    print()    
+    choice = input("Do you want to still continue to read the story? (yes/no): ")
+    if choice.lower() == "yes": 
+        print()    
+        print("When minions leave for New York city."
+            + " Labubus king, whose name is Zimomo, wants to make his kingdom big."
+            + " He is an enemy of minions but minions do not let Labubus take over "
+            + " their ice cave. He gave command to his soldiers to attack on"
+            + " home. When the minions figured out that Zimomo took"
+            + " over their house."
+            + "Minion came back with their new boss to fight with Labubus.")
+
+story_lines2()
