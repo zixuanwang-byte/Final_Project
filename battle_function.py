2383000c860c877d3901f6e2f5f7a198c0721687
twishha6272727 <twishhamayur.patel1@rbe.sk.ca>
2026-06-15 14:39:38 -0600

    change to right code
    this is copied from version 1's code

--- battle_function.py
+++ battle_function.py
@@ -1,17 +1,44 @@
-# battle functions
+#Battle Function
+from character import Player, Enemy
+from moves import Moves
+
+fire = Moves("Fire", [1, 5])
+water = Moves("Water", [1, 5])
+iron = Moves("Iron", [1, 5])
+cannon = Moves("Cannon", [1, 5])
+bubble = Moves("Bubble", [1, 5])
+
+player_moves = {
+    "Fire": fire,
+    "Water": water,
+    "Iron": iron,
+    "Cannon": cannon,
+    "Bubble": bubble
+}
+
+enemy_moves = [fire, water, iron, cannon, bubble]
+
+Minion = Player("minion", True, 10, 10, player_moves, 10)
+Labubu = Enemy("Labubu", True, 10, 10, enemy_moves, 10)
+
 def battle(player, enemy):
     print(f"{player.name} is battling with {enemy.name}.")
-    while self.health > 0 and enemy.health > 0 :
-        enemy.health -= self.attacks
-        print(f"{self.name} do the attacks on {enemy.name}.")
+
+    while player.health > 0 and enemy.health > 0:
+
+        # Player turn
+        player.use_move(enemy)
 
         if enemy.health <= 0:
-            print(f"{enemy.name} was loose in this battle.")
+            print(f"{enemy.name} was defeated!")
             break
-        
-        self.health -= enemy.attack
-        print(f"{enemy.name} attacks on you.")
 
-        if self.health <= 0:
-            print(f"{self.name} was loose in this battle.")
-    
+        # Enemy turn
+        enemy.attack(player)
+
+        if player.health <= 0:
+            print(f"{player.name} was defeated!")
+            break
+
+
+battle(Minion, Labubu)
