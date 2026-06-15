91333e8406ae2d6dba1023b09dd4c9e365958047
twishha6272727 <twishhamayur.patel1@rbe.sk.ca>
2026-06-15 15:06:40 -0600

    map

---
+++ map.py
@@ -0,0 +1,9 @@
+from tabulate import tabulate
+
+map = [["Zimomo's soldiers", "", "Artic Battlefeild", "", "Minions"],
+    ["Zimomo's soldiers", "Zimomo", "Artic Battlefeild", "Scarlett Overkill", "Minions"],
+    ["Zimomo's soldiers", "", "Artic Battlefeild", "", "Minions"]
+    ]
+headers = ["  West", "", "  Center","", "  East"]
+
+print(tabulate(map, headers=headers, tablefmt="fancy_grid", stralign="center"))
